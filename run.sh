82ab705458c172b3584321fde331e3e7f1a6057b
Christian Klem <christian.klem@nav.no>
2025-07-01 13:24:51 +0200

    Oppdater pdfgen baseimage

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
-PDFGEN_VERSION="2.0.80"
+PDFGEN_VERSION="2.0.84"
 
 docker pull ghcr.io/navikt/pdfgen:$PDFGEN_VERSION
 docker run \
